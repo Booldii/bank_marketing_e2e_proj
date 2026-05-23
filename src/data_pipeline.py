99e2cd307907896e273395c2e06b9ff98452389e
Booldii <wojtek.adamczyk95@gmail.com>
2026-05-23 22:04:49 +0200

    data_pipe optimized

--- src/data_pipeline.py
+++ src/data_pipeline.py
@@ -1,13 +1,13 @@
 import pandas as pd
 import numpy as np
-from scipy.stats import mstats
 
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-from sklearn.preprocessing import FunctionTransformer, OrdinalEncoder
+from sklearn.preprocessing import OneHotEncoder, FunctionTransformer, OrdinalEncoder
 from sklearn.impute import SimpleImputer
+from sklearn.base import BaseEstimator, TransformerMixin
+from category_encoders import OrdinalEncoder as ce_encoder
 
 
 NUMERIC_COLS = [
@@ -18,83 +18,80 @@ OHE_COLS = [
     'job', 'marital', 'housing', 'loan',
     'contact', 'day_of_week', 'poutcome'
 ]
-
 WINSORIZE_COLS = ['age', 'campaign']
 
-EDUCATION_MAP = {
-    'illiterate': 0, 'basic.4y': 0, 'basic.6y': 0, 'basic.9y': 0,
-    'high.school': 1, 'professional.course': 2, 'university.degree': 3,
-    np.nan: np.nan
+class WinsorizerTransformer(BaseEstimator, TransformerMixin):
+    def __init__(self, limits=(0.001, 0.001)):
+        self.limits = limits
+        self.lower_bounds_ = None
+        self.upper_bounds_ = None
+
+    def fit(self, X, y=None):
+        X_df = pd.DataFrame(X)
+        self.lower_bounds_ = X_df.quantile(self.limits[0]).values
+        self.upper_bounds_ = X_df.quantile(1 - self.limits[1]).values
+        return self
+
+    def transform(self, X, y=None):
+        X_copy = X.copy()
+        for i in range(X_copy.shape[1]):
+            X_copy[:, i] = np.clip(X_copy[:, i],self.lower_bounds_[i], self.upper_bounds_[i])
+        return X_copy
+
+
+EDU_MAPPING= {
+        'illiterate': 0,
+        'basic.4y': 0,
+        'basic.6y': 0,
+        'basic.9y': 0,
+        'high.school': 1,
+        'professional.course': 2,
+        'university.degree': 3,
+        np.nan: np.nan
 }
 
-MONTH_MAP = {
-    'jan': 1, 'feb': 2, 'mar': 3, 'apr': 4, 'may': 5, 'jun': 6,
-    'jul': 7, 'aug': 8, 'sep': 9, 'oct': 10, 'nov': 11, 'dec': 12,
-    np.nan: np.nan
-}
+MONTH_CATS = [
+    'jan', 'feb', 'mar', 'apr', 'may', 'jun',
+    'jul', 'aug', 'sep', 'oct', 'nov', 'dec'
+]
 
-def apply_winsorization(X, limits=(0.001, 0.001)):
-    X_copy = np.copy(X)
-    for i in range(X_copy.shape[1]):
-        X_copy[:, i] = mstats.winsorize(X_copy[:, i], limits=limits)
-    return X_copy
+def get_preprocessor():
+    """"""
 
-def map_ordinal_features(X, mapping_dict):
-    return pd.DataFrame(X).replace(mapping_dict).values.astype(float)
-
-numeric_pipeline = Pipeline(
-    steps=[
+    numeric_pipeline = Pipeline(steps=[
         ('imputer', SimpleImputer(strategy='median'))
-    ]
-)
+    ])
 
-winsorize_pipeline = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='median')),
-    ('winsorizer', FunctionTransformer(apply_winsorization, kw_args={'limits': (0.001, 0.001)}))
-])
+    winsorize_pipeline = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='median')),
+        ('winsorizer', WinsorizerTransformer(limits=(0.001, 0.001))),
+    ])
 
-ohe_pipeline = Pipeline(steps=[
-    ('imputer', SimpleImputer(strategy='most_frequent')),
-    ('ohe', OneHotEncoder(sparse_output=False, handle_unknown='ignore'))
-])
+    ohe_pipeline = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent')),
+        ('ohe', OneHotEncoder(sparse_output=False, handle_unknown='ignore'))
+    ])
 
-edu_pipeline = Pipeline(steps=[
-    ('mapper', FunctionTransformer(map_ordinal_features, kw_args={'mapping_dict': EDUCATION_MAP})),
-    ('imputer', SimpleImputer(strategy='median'))
-])
+    edu_pipeline = Pipeline(steps=[
+        ('imputer', SimpleImputer(strategy='most_frequent').set_output(transform='pandas')),
+        ('encoder', ce_encoder(mapping=[{'col': 'education', 'mapping': EDU_MAPPING}], handle_unknown='ignore'))
+    ])
 
-month_pipeline = Pipeline(steps=[
-    ('mapper', FunctionTransformer(map_ordinal_features, kw_args={'mapping_dict': MONTH_MAP})),
-    ('imputer', SimpleImputer(strategy='most_frequent'))
-])
+    month_pipeline = Pipeline(steps=[
+        ('encoder', OrdinalEncoder(categories=[MONTH_CATS], handle_unknown='use_encoded_value', unknown_value=np.nan)),
+        ('imputer', SimpleImputer(strategy='most_frequent'))
+    ])
 
-num_no_winsorize = [col for col in NUMERIC_COLS if col not in WINSORIZE_COLS]
+    num_no_winsorize = [col for col in NUMERIC_COLS if col not in WINSORIZE_COLS]
 
-ct = ColumnTransformer(
-    transformers=[
-        ('num_std', numeric_pipeline, num_no_winsorize),
-        ('num_win', winsorize_pipeline, WINSORIZE_COLS),
-        ('cat_ohe', ohe_pipeline, OHE_COLS),
-        ('cat_edu', edu_pipeline, ['education']),
-        ('cat_month', month_pipeline, ['month'])
-    ],
-    remainder='passthrough'
-)
-
-if __name__ == "__main__":
-    df = pd.read_csv(r'../data/raw/bank-additional-full.csv', sep = ';')
-    df['y'] = np.where(df['y'] == 'yes', 1, 0)
-
-    df = df.replace('unknown', np.nan)
-
-    X_cols = NUMERIC_COLS + OHE_COLS + ['education', 'month']
-    X = df[X_cols]
-    y = df['y']
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-    X_train_processed = ct.fit_transform(X_train)
-    X_test_processed = ct.transform(X_test)
-
-    print(f"Kształt X_train przed: {X_train.shape}")
-    print(f"Kształt X_train po transformacjach: {X_train_processed.shape}")
+    ct = ColumnTransformer(
+        transformers=[
+            ('num_std', numeric_pipeline, num_no_winsorize),
+            ('num_win', winsorize_pipeline, WINSORIZE_COLS),
+            ('cat_ohe', ohe_pipeline, OHE_COLS),
+            ('cat_edu', edu_pipeline, ['education']),
+            ('cat_month', month_pipeline, ['month'])
+        ],
+        remainder='passthrough'
+    )
+    return ct
